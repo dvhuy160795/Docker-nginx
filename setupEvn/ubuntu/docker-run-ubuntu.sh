ccc6f3932ebe51c69d278ad0f4c8e4477dc688bc
dvhuy160795 <dvhuy160795@gmail.com>
2020-01-30 11:17:36 +0700

    fix evn

--- setupEvn/ubuntu/docker-run-ubuntu.sh
+++ setupEvn/ubuntu/docker-run-ubuntu.sh
@@ -3,12 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 SCRIPT=$(readlink -f $0)
 SOURCEPATH=`dirname $SCRIPT`
 
-composer create-project --prefer-dist laravel/laravel $SOURCEPATH/../../source
-
-
 chmod -R 777 $SOURCEPATH/../../source/storage/
-(cd $SOURCEPATH/../../source && exec composer install)
-(cd $SOURCEPATH/../../source && exec chmod -R 777 storage/)
 
 #run docker-compose
 docker rm -f huydv-mysql server-nginx huydv-php
